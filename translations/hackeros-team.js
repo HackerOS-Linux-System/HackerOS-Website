e50748f0e926c6de7e607d4ad6da23be0f21ba43
michal92299 <chudybyczek2137@gmail.com>
2026-04-25 11:59:58 +0200

    Create hackeros-team.js

---
+++ translations/hackeros-team.js
@@ -0,0 +1,24 @@
+window.HACKEROS_TRANS_TEAM = {
+    pl: {
+        pageTitle:   'Zespół HackerOS',
+        sectionH1:   'Zespół HackerOS',
+        member0Role: 'Założyciel HackerOS',
+        footerCopy:  '© 2026 HackerOS. Wszelkie prawa zastrzeżone.',
+        navHome:     'Home',
+        navDownload: 'Download',
+        navReleases: 'Releases',
+        navTeam:     'HackerOS Team',
+        navDocs:     'Documentation',
+    },
+    en: {
+        pageTitle:   'HackerOS Team',
+        sectionH1:   'HackerOS Team',
+        member0Role: 'Founder of HackerOS',
+        footerCopy:  '© 2026 HackerOS. All rights reserved.',
+        navHome:     'Home',
+        navDownload: 'Download',
+        navReleases: 'Releases',
+        navTeam:     'HackerOS Team',
+        navDocs:     'Documentation',
+    }
+};
