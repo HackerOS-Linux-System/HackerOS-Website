316815ac54caeb8e640f3ddf4ade8cfc44740c36
michal92299 <chudybyczek2137@gmail.com>
2026-08-07 21:09:42 +0200

    Create download-editions.js

---
+++ translations/download-editions.js
@@ -0,0 +1,118 @@
+window.HACKEROS_DOWNLOAD_EDITIONS = [
+    {
+        id: 'official',
+        name: 'HackerOS Official',
+        img: 'official-edition.png',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/OFFICIAL/HackerOS-V4.9.iso/download',
+            mega:     'https://mega.nz/file/8ftCRYQa#jIIOe8db_gPtGroe68QdsF5-3wfiudxhcQUff0wv99w',
+            drive:    'https://drive.google.com/file/d/1aX7N1_bDpzWCVBictAP8OyhBQk5doa1X/view?usp=sharing',
+            transfer: 'https://transfer.it/t/snqZUH0VnTqL',
+            actions:  'https://github.com/HackerOS-Linux-System/HackerOS/actions/runs/31165869861/artifacts/8989604188'
+        }
+    },
+    {
+        id: 'nvidia',
+        name: 'HackerOS NVIDIA',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/NVIDIA/HackerOS-V4.9-NVIDIA.iso/download',
+            mega:     'https://mega.nz/file/0XNGESLD#QNzrTswUsmUUCBM6Ua4hBQ7R2Vg1PyAgiEV04BHgIwU',
+            drive:    'https://drive.google.com/file/d/1CU6Lc2XoCmVqPx6OwV046WaxN9DiHcW4/view?usp=sharing',
+            transfer: 'https://transfer.it/t/m5ubFetnhJSI',
+            actions:  'https://github.com/HackerOS-Linux-System/HackerOS/actions/runs/31165869861/artifacts/8989674334'
+        }
+    },
+    {
+        id: 'cybersecurity',
+        name: 'HackerOS Cybersecurity',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/CYBERSECURITY/HackerOS-V4.9-Cybersecurity.iso/download',
+            mega:     'https://mega.nz/file/UCVn3T7Y#OCT69qU6O8V7GxoyK__5T3jkHouLhLRB_0ZyMF6rWWo',
+            drive:    'https://drive.google.com/file/d/1q6ToTXs9K0Ce4TykLwlSw0-MREO7bnfb/view?usp=sharing',
+            transfer: 'https://transfer.it/t/ArXx0GtTVbCK',
+            actions:  'https://github.com/HackerOS-Linux-System/HackerOS/actions/runs/31165869861/artifacts/8989694184'
+        }
+    },
+    {
+        id: 'gnome',
+        name: 'HackerOS Official: Gnome',
+        img: 'gnome-edition.png',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/OFFICIAL/GNOME/HackerOS-V4.5-Gnome.iso/download',
+            mega:     'https://mega.nz/file/dbVhHA4b#XX_gWHN4leztjswMtl68xYZ9m4R4tYbwd0atSqQ-mDE',
+            drive:    'https://drive.google.com/file/d/1_oz4vywV-L5FuGE_t-b-PcFTcmtpeKd3/view?usp=sharing',
+            transfer: 'https://transfer.it/t/ZjwA5f1lzbjS',
+            actions:  null
+        }
+    },
+    {
+        id: 'hydra',
+        name: 'HackerOS Official: Hydra',
+        img: 'hydra-edition.png',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/OFFICIAL/HYDRA/HackerOS-V4.5-Hydra.iso/download',
+            mega:     'https://mega.nz/file/ITUQQL5Z#t5vDdYxOiOqgU1bS3BJboKlnX7N1OwKFV3-LE6Srg9s',
+            drive:    'https://drive.google.com/file/d/1jxQLxomUzD8t9jHjnoB3pU2XnpTrdqOG/view?usp=sharing',
+            transfer: 'https://transfer.it/t/LEeOtbsFmRIT',
+            actions:  null
+        }
+    },
+    {
+        id: 'xfce',
+        name: 'HackerOS Official: Xfce',
+        img: 'xfce-edition.png',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/OFFICIAL/XFCE/HackerOS-V4.5-Xfce.iso/download',
+            mega:     null,
+            drive:    'https://drive.google.com/file/d/1g3mYZIVHZ7L_see5aR29mgjLOQBhgoaq/view?usp=sharing',
+            transfer: null,
+            actions:  null
+        }
+    },
+    {
+        id: 'blue',
+        name: 'HackerOS Blue',
+        img: 'blue-edition.png',
+        isNew: true,
+        links: {
+            sf:       null,
+            mega:     null,
+            drive:    null,
+            transfer: null,
+            actions:  null
+        }
+    },
+    {
+        id: 'gaming',
+        name: 'HackerOS Gaming',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/GAMING/HackerOS-V4.7-Gaming.iso/download',
+            mega:     null,
+            drive:    null,
+            transfer: null,
+            actions:  null
+        }
+    },
+    {
+        id: 'lts',
+        name: 'HackerOS LTS',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/LTS/HackerOS-V4.1-LTS.iso/download',
+            mega:     null,
+            drive:    'https://drive.google.com/file/d/1Kc-L3gFuXP7Vw-r-kVWgSscxDm5GW0QX/view?usp=sharing',
+            transfer: null,
+            actions:  null
+        }
+    },
+    {
+        id: 'atomic',
+        name: 'HackerOS Atomic',
+        links: {
+            sf:       'https://sourceforge.net/projects/hackeros/files/ATOMIC/HackerOS-Atomic-Testing-DEV-V4.9.iso/download',
+            mega:     null,
+            drive:    'https://drive.google.com/file/d/1qizajIMGKG8_N5EtOuzD2IQndfjDSUV_/view?usp=sharing',
+            transfer: null,
+            actions:  'https://github.com/HackerOS-Linux-System/HackerOS/actions/runs/31167851135/artifacts/8990275885'
+        }
+    }
+];
