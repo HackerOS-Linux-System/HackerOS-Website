0b6edd0ba1f18a7637d5aeed28a3ba1e261fa04d
michal92299 <chudybyczek2137@gmail.com>
2026-08-08 23:10:44 +0200

    Create pl.js

---
+++ translations/files/all/pl.js
@@ -0,0 +1,123 @@
+window.HACKEROS_RELEASES_ALL = window.HACKEROS_RELEASES_ALL || {};
+window.HACKEROS_RELEASES_ALL.pl = [
+    {
+        version: "HackerOS V4.9",
+        desc: "Nowa edycja z wieloma usprawnieniami.",
+        dates: ["HackerOS Official: 7.08.2026", "HackerOS Cybersecurity: 7.08.2026", "HackerOS NVIDIA: 7.08.2026"],
+        changelog: ["Zaktualizowano i rozbudowano tło bootloadera w trybie live.", "Dodano własny motyw Plymouth.", "Zaktualizowano Hacker Lang.", "Zaktualizowano Hacker Launcher.", "Zaktualizowano i rozbudowano HackerOS Store.", "Całkowite przeniesienie budowania obrazów ISO z lokalnego na GitHub Actions – obrazy ISO są od teraz budowane w GitHub Actions."]
+    },
+    {
+        version: "HackerOS V4.8",
+        desc: "Nowa edycja z zaktualizowanymi narzędziami.",
+        dates: ["HackerOS Official: 30.06.2026", "HackerOS Cybersecurity: 19.06.2026"],
+        changelog: ["Zaktualizowano i rozbudowano HackerOS Games.", "Rozbudowano narzędzie CLI hacker.", "Aktualizacja konfiguracji."]
+    },
+    {
+        version: "HackerOS V4.7",
+        desc: "Nowa edycja z poprawkami oraz aktualizacjami.",
+        dates: ["HackerOS Official: 1.06.2026", "HackerOS Cybersecurity: 2.06.2026", "HackerOS Gaming: 3.06.2026"],
+        changelog: ["Naprawiono błędy z autologowaniem.", "Zaktualizowano narzędzia CLI/GUI.", "Dodano nową edycję Gaming."]
+    },
+    {
+        version: "HackerOS V4.6",
+        desc: "Nowa edycja z zaktualizowanymi narzędziami CLI oraz zaktualizowanym motd.",
+        dates: ["HackerOS Official: 6.05.2026", "HackerOS NVIDIA: 5.05.2026", "HackerOS Cybersecurity: 5.05.2026"],
+        changelog: ["Rozbudowano narzędzia: hacker, Hacker Lang (narzędzie hl), hdev, hnm, hpm, hedit", "Zaktualizowano motd"]
+    },
+    {
+        version: "HackerOS V4.5",
+        desc: "Nowa edycja głównie z poprawą narzędzi.",
+        dates: ["HackerOS Official: 6.04.2026", "HackerOS NVIDIA: 5.04.2026", "HackerOS Cybersecurity: 4.04.2026", "HackerOS Gnome: 4.04.2026", "HackerOS Hydra: 3.04.2026", "HackerOS Xfce: 3.04.2026"],
+        changelog: ["Naprawiono błędy w narzędziach: HackerOS-Steam, hacker", "Zaktualizowano narzędzia CLI", "Dodano obsluge kilku języków (domyślnie polski ale można zmienić) dla narzędzia hacker", "Zaktualizowano motd"]
+    },
+    {
+        version: "HackerOS V4.4",
+        desc: "Nowa edycja głównie z poprawkami.",
+        dates: ["HackerOS Official: 6.03.2026", "HackerOS NVIDIA: 7.03.2026", "HackerOS Cybersecurity: 8.03.2026"],
+        changelog: ["Naprawiono błędy w narzędziach z AppImage", "Zaktualizowano narzędzia CLI", "Zaktualizowano język programowania HackerOS"]
+    },
+    {
+        version: "HackerOS V4.3",
+        desc: "Nowa wersja z zaktualizowaną edycją cybersecurity oraz narzędziami GUI.",
+        dates: ["HackerOS Official: 6.02.2026", "HackerOS Cybersecurity: 7.02.2026", "HackerOS NVIDIA: 7.02.2026"],
+        changelog: ["Zaktualizowano narzędzia CLI", "Zaktualizowano narzędzia GUI", "Nowy prerelease dla edycji atomic", "Dodano nową edycję NVIDIA"]
+    },
+    {
+        version: "HackerOS V4.2",
+        desc: "Nowa wersja z zaktualizowanymi narzędziami CLI i nowymi funkcjami.",
+        dates: ["HackerOS Official: 18.01.2026", "HackerOS Cybersecurity: 18.01.2026"],
+        changelog: ["Zaktualizowano narzędzia CLI", "Dodano nową aplikację HackerOS App", "Naprawiono narzędzie CLI o nazwie HackerOS-Steam", "Dodano prerelease dla edycji atomic", "Zaktualizowano edycję cybersecurity o nowe narzędzia i lepszą przeglądarkę"]
+    },
+    {
+        version: "HackerOS V4.1",
+        desc: "Nowa wersja z naprawionymi błędami i nowymi funkcjami.",
+        dates: ["HackerOS Official: 26.12.2025", "HackerOS Hydra: 26.12.2025", "HackerOS Cybersecurity: 26.12.2026", "HackerOS LTS: 26.12.2025"],
+        changelog: ["Naprawiono błędy z MOTD", "Dodano nową edycję LTS", "Zaktualizowano bazę Debiana do Debian Forky", "Dodano zaktualizowane narzędzia"]
+    },
+    {
+        version: "HackerOS V4.0",
+        desc: "Nowa wersja z zaktualizowanymi narzędziami oraz aplikacjami.",
+        dates: ["HackerOS Official: 8.12.2025", "HackerOS Official Gnome: 12.12.2025", "HackerOS Official Xfce: 12.12.2025", "HackerOS Hydra: 8.12.2025", "HackerOS Cybersecurity: 9.12.2025"],
+        changelog: ["Zaktualizowano narzędzia hacker, hli, hackerc, bytes, hpm oraz aplikacje", "Dodano MOTD"]
+    },
+    {
+        version: "HackerOS V3.9",
+        desc: "Nowa wersja z rozbudowanymi narzędziami.",
+        dates: ["HackerOS Official: 22.11.2025", "HackerOS Hydra: 22.11.2025", "HackerOS Cybersecurity: 23.11.2025"],
+        changelog: ["Zaktualizowano bazę do najnowszego Debiana.", "Rozbudowano skrypty: unpack-xanmod i unpack-liquorix. Rozbudowano narzędzie hacker oraz zaktualizowano aplikację update system."]
+    },
+    {
+        version: "HackerOS V3.8",
+        desc: "Nowa wersja z zaktualizowanymi narzędziami i naprawionymi skryptami.",
+        dates: ["HackerOS Official: 16.11.2025", "HackerOS Hydra: 16.11.2025", "HackerOS Cybersecurity: 16.11.2025"],
+        changelog: ["Zaktualizowano narzędzia - hacker, hackerc.", "Naprawiono skrypty."]
+    },
+    {
+        version: "HackerOS V3.7",
+        desc: "Nowa wersja z naprawionymi skryptami oraz rozbudowanymi narzędziami hacker, hackerc",
+        dates: ["HackerOS Official: 10.11.2025", "HackerOS Hydra: 10.11.2025", "HackerOS Cybersecurity: 10.11.2025"],
+        changelog: ["Zaktualizowano bazę do najnowszego Debiana, dodano najnowsze narzędzia: HackerOS Welcome, hacker oraz narzędzia dla języka programowania hacker - hackerc", "Naprawiono skrypty uruchamiające hacker mode"]
+    },
+    {
+        version: "HackerOS V3.6",
+        desc: "Nowa wersja z naprawionymi błędami i modyfikacjami",
+        dates: ["HackerOS Official: 07.11.2025", "HackerOS Hydra: 07.11.2025", "HackerOS Cybersecurity: 07.11.2025"],
+        changelog: ["Zaktualizowano bazę do najnowszego Debiana", "Naprawiono błędy w aplikacji hacker launcher", "Naprawiono błędy w hacker mode", "Naprawiono/Rozbudowano skrypty"]
+    },
+    {
+        version: "HackerOS V3.5",
+        desc: "Nowa wersja z aktualizacją bazy, dodanym językiem programowania hacker lang oraz innymi zmianami.",
+        dates: ["HackerOS Official: 29.10.2025", "HackerOS Hydra: 29.10.2025", "HackerOS Cybersecurity: 29.10.2025"],
+        changelog: ["Zaktualizowano bazę do najnowszego Debiana", "Dodano język programowania hacker lang", "Zaktualizowano hacker mode aby działał tylko i wyłącznie na x11 (hacker mode będzie działać zarówno na nowych gpu jak i na starych)", "Zaktualizowano permisje dla plików"]
+    },
+    {
+        version: "HackerOS V3.4",
+        desc: "Usunięto edycję Nvidia, wiele zmian.",
+        dates: ["HackerOS Official: 24.10.2025", "HackerOS Hydra: 24.10.2025", "HackerOS Cybersecurity: 24.10.2025"],
+        changelog: ["Usunięto narzędzie hacker-update", "Od teraz nie będzie wbudowanego jądra xanmod, zainstaluj je i ustaw jako domyślne za pomocą polecenia set-xanmod-default", "Rozbudowano narzędzie hacker", "Usunięto gry HackerOS, dodano jedną aplikację która uruchamia wszystkie gry HackerOS", "Od teraz hacker mode nie będzie odpalał za pomocą gamescope tylko cage", "Dodano aplikację inspirowaną ASUS Armoury Crate o nazwie HackerOS Game Mode"]
+    },
+    {
+        version: "HackerOS V3.3",
+        desc: "Dodano nową edycję Nvidia.",
+        dates: ["HackerOS Official: 14.10.2025", "HackerOS Hydra: 14.10.2025", "HackerOS Cybersecurity: 14.10.2025", "HackerOS Nvidia: 14.10.2025"],
+        changelog: ["Zaktualizowano bazę do najnowszego Debiana.", "Rozbudowano Hacker Launcher, gotowy do uruchamiania gier i plików .exe.", "Naprawiono i przebudowano Hacker Mode.", "Zaktualizowano skrypty."]
+    },
+    {
+        version: "HackerOS V3.2",
+        desc: "Dodano nową edycję Hydra Edition oraz zaktualizowano tapety.",
+        dates: ["HackerOS Official: 29.09.2025", "HackerOS Hydra: 29.09.2025", "HackerOS Cybersecurity: 30.09.2025"],
+        changelog: ["Zaktualizowana baza do najnowszego Debian Testing.", "Zaktualizowano tapety.", "Zaktualizowano skrypty.", "Naprawiono kilka błędów."]
+    },
+    {
+        version: "HackerOS V3.1",
+        desc: "Najnowsza wersja, poprawiono wiele znanych błędów z HackerOS V3.0.",
+        dates: ["HackerOS Official: 20.09.2025", "HackerOS Cybersecurity: 20.09.2025"],
+        changelog: ["Zaktualizowana baza do najnowszego Debian Testing.", "Naprawiono błędy z tapetami.", "Naprawiono błędy z jądrem.", "Dodano nową grę The Racer i nową aplikację HackerOS Welcome."]
+    },
+    {
+        version: "HackerOS V3.0",
+        desc: "Najnowsza główna wersja z znacznymi ulepszeniami: zmieniono z jądra xanmod na xanmod lts (lepsza obsługa urządzeń).",
+        dates: ["HackerOS Official: 13.09.2025", "HackerOS Nvidia: 13.09.2025", "HackerOS Cybersecurity: 13.09.2025"],
+        changelog: ["Zaktualizowano bazę do najnowszego Debian Testing.", "Poprawiono wsparcie dla sterowników Nvidia dla lepszej wydajności w grach.", "Dodano zaawansowane narzędzia cybersecurity, takie jak ulepszone firewalle i wykrywanie intruzów.", "Poprawki błędów, dodano i ulepszono narzędzia cli oraz gry hackeros.", "Poprawki błędów i ulepszenia stabilności na podstawie opinii użytkowników."]
+    },
+];
