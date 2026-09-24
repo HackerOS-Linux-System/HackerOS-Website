b7683c82b38e6e390822196640ac1308e7922968
michal92299 <chudybyczek2137@gmail.com>
2026-09-24 21:34:17 +0200

    Create index.js

---
+++ articles/index.js
@@ -0,0 +1,47 @@
+window.HACKEROS_ARTICLES_INDEX = {
+ "version": 1,
+ "tags": {
+  "programming": {
+   "pl": "Programowanie",
+   "en": "Programming"
+  },
+  "tools": {
+   "pl": "Narzędzia",
+   "en": "Tools"
+  },
+  "guide": {
+   "pl": "Poradnik",
+   "en": "Guide"
+  },
+  "editions": {
+   "pl": "Edycje",
+   "en": "Editions"
+  },
+  "gaming": {
+   "pl": "Gry",
+   "en": "Gaming"
+  },
+  "system": {
+   "pl": "System",
+   "en": "System"
+  }
+ },
+ "articles": [
+  {
+   "id": "hsharp",
+   "file": "hsharp.js"
+  },
+  {
+   "id": "hackeros-editions",
+   "file": "hackeros-editions.js"
+  },
+  {
+   "id": "gaming-on-hackeros",
+   "file": "gaming-on-hackeros.js"
+  },
+  {
+   "id": "first-steps",
+   "file": "first-steps.js"
+  }
+ ]
+};
